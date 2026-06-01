33326238867e088d650cfcee5c28413952895195
SuryatejaAdaveni <asurya2022@gmail.com>
2026-06-01 09:38:12 +0530

    first commit

---
+++ Java/Child.java
@@ -0,0 +1,15 @@
+class Father {
+	Father() {
+		System.out.println("Hi");
+	}
+}
+
+class Child extends Father {
+	public Child() {
+		super();
+	}
+
+	public static void main(String[] args) {
+
+	}
+}
