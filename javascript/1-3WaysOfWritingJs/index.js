9b611f844472882133c545de42cb0718bdb6fa24
SuryatejaAdaveni <asurya2022@gmail.com>
2026-07-24 19:14:14 +0530

    JavaScript

---
+++ javascript/1-3WaysOfWritingJs/index.js
@@ -0,0 +1,9 @@
+// let btn = document.querySelector("button");
+// btn.addEventListener("click", () => {
+//   document.querySelector("body").style.backgroundColor = "red";
+// });
+
+var a = 10;
+a = 20;
+var a = 30;
+console.log(a);
